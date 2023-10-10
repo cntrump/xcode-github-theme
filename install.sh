38c1268015de30ae6756fcc02feb49dd792f5b8e
Vinayak Srivastava <vinayaks@vmware.com>
2023-10-10 11:49:41 +0530

    Added fonts and updated installation script

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 set -eux
 
+fontsDir=~/Library/Fonts
+cp -f *.ttf ${themeDir}
+
 themeDir=~/Library/Developer/Xcode/UserData/FontAndColorThemes
 mkdir -p ${themeDir}
 cp -f *.xccolortheme ${themeDir}
